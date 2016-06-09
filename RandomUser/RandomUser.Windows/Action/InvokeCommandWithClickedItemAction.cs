da41be854dea07a76f8e2d8a814dc7269a722565
Sebastian Jensen <github@tsjdev-apps.de>
2016-06-09 20:35:55 +0200

    - add action

---
+++ RandomUser/RandomUser.Windows/Action/InvokeCommandWithClickedItemAction.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Microsoft.Xaml.Interactivity;
+
+namespace RandomUser.Windows.Action
+{
+    public class InvokeCommandWithClickedItemAction : DependencyObject, IAction
+    {
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(InvokeCommandWithClickedItemAction), new PropertyMetadata(null));
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public object Execute(object sender, object parameter)
+        {
+            if (Command != null && parameter is ItemClickEventArgs)
+                Command.Execute(((ItemClickEventArgs)parameter).ClickedItem);
+
+            return null;
+        }
+    }
+}
+
