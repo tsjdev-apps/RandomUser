f76351fd0ae9a205cd5a3e3706d26d70ffa3c1a5
Sebastian Jensen <github@tsjdev-apps.de>
2016-06-09 20:08:58 +0200

    - add extension methods

---
+++ RandomUser/RandomUser.Core/ExtensionMethods/CollectionExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace RandomUser.Core.ExtensionMethods
+{
+    public static class CollectionExtensions
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> collection)
+        {
+            return collection == null ? new ObservableCollection<T>() : new ObservableCollection<T>(collection);
+        }
+
+        public static bool HasItems<T>(this IEnumerable<T> collection)
+        {
+            return collection.IsNotNull() && collection.Any();
+        }
+
+        public static bool IsEmtpy<T>(this IEnumerable<T> collection)
+        {
+            return collection.IsNotNull() && !collection.Any();
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
+        {
+            foreach (var item in enumeration)
+            {
+                action(item);
+            }
+        }
+
+        public static void AddRange<T>(this IList<T> list, IEnumerable<T> collection)
+        {
+            foreach (var item in collection)
+                list.Add(item);
+        }
+
+        public static void Filter<T>(this IList<T> collection, IEnumerable<T> filteredCollection)
+        {
+            var enumerable = filteredCollection as T[] ?? filteredCollection.ToArray();
+            var itemsToRemove = collection.Except(enumerable).ToArray();
+
+            foreach (var item in itemsToRemove)
+                collection.Remove(item);
+
+            foreach (var item in enumerable.Where(item => !collection.Contains(item)))
+                collection.Add(item);
+        }
+    }
+}
---
+++ RandomUser/RandomUser.Core/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RandomUser.Core.ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        public static bool IsNeitherNullNorEmpty(this string input)
+        {
+            return !string.IsNullOrEmpty(input);
+        }
+
+        public static bool IsNullOrEmpty(this string input)
+        {
+            return string.IsNullOrEmpty(input);
+        }
+
+        public static string FirstCharToUpper(this string input)
+        {
+            if (input.IsNullOrEmpty())
+                throw new ArgumentNullException(nameof(input));
+
+            return input[0].ToString().ToUpper() + input.Substring(1);
+        }
+    }
+}
