d90302d04cdbf71ca27bd8fedc1646689ce6a863
Sebastian Jensen <github@tsjdev-apps.de>
2016-06-09 20:09:36 +0200

    - add model class

---
+++ RandomUser/RandomUser.Core/Model/User.cs
@@ -0,0 +1,14 @@
+﻿namespace RandomUser.Core.Model
+{
+    public class User
+    {
+        public string Gender { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Cell { get; set; }
+        public string PictureUrl { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
