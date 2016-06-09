7a99bcb06d90361862f76f69827ca2c3ace0d69e
Sebastian Jensen <github@tsjdev-apps.de>
2016-06-09 20:36:52 +0200

    - add bootstrapper

---
+++ RandomUser/RandomUser.Windows/Init/BootStrapper.cs
@@ -0,0 +1,37 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Views;
+using Microsoft.Practices.ServiceLocation;
+using RandomUser.Core.Interfaces.Repository;
+using RandomUser.Core.Interfaces.Service;
+using RandomUser.Core.Repository;
+using RandomUser.Core.Service;
+using RandomUser.Core.ViewModel;
+using RandomUser.Windows.Service;
+
+namespace RandomUser.Windows.Init
+{
+    public class BootStrapper
+    {
+        public BootStrapper()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            // Register Services
+            SimpleIoc.Default.Register<IDataService, DataService>();
+            SimpleIoc.Default.Register<IDialogService, DialogService>();
+            SimpleIoc.Default.Register<IHttpClientService, HttpClientService>();
+            SimpleIoc.Default.Register<IMappingService, MappingService>();
+            SimpleIoc.Default.Register<IResourceService, ResourceService>();
+            
+            // Register Repositories
+            SimpleIoc.Default.Register<IUserRepository, UserRepository>();
+
+            // Register ViewModels
+            SimpleIoc.Default.Register<UserOverviewViewModel>();
+            SimpleIoc.Default.Register<UserDetailViewModel>();
+        }
+
+        public UserOverviewViewModel UserOverviewViewModel => SimpleIoc.Default.GetInstance<UserOverviewViewModel>();
+        public UserDetailViewModel UserDetailViewModel => SimpleIoc.Default.GetInstance<UserDetailViewModel>();
+    }
+}
