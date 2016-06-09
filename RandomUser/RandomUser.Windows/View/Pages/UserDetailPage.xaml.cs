1a85abb3cf4a4fe09479a9559c9a78cdb5210d35
Sebastian Jensen <github@tsjdev-apps.de>
2016-06-09 20:48:33 +0200

    - add view pages

---
+++ RandomUser/RandomUser.Windows/View/Pages/UserDetailPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace RandomUser.Windows.View.Pages
+{
+    public sealed partial class UserDetailPage : Page
+    {
+        public UserDetailPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ RandomUser/RandomUser.Windows/View/Pages/UserOverviewPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace RandomUser.Windows.View.Pages
+{
+    public sealed partial class UserOverviewPage : Page
+    {
+        public UserOverviewPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
